496783f1916338b6f1c5d3e10e4479344bb0a501
pappala343 <chandramouli.pappala@gmail.com>
2023-11-30 21:29:36 -0500

    Tfa changes (#11)
    * TFA Changes
    
    * MInor changes in server.js file
    
    * minor changes
    
    * removed unnecessary code
    
    * front end UI changes

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mysql = require('mysql2');
 const path = require('path')
 const moment = require('moment');
+const session = require('express-session');
+const speakeasy = require('speakeasy');
+const QRCode = require('qrcode');
 require('dotenv').config();
 
 const password = process.env.dbPassword;
@@ -18,6 +21,12 @@ const connection = mysql.createConnection({
 
 const exphbs = require('express-handlebars');
 const handlebars = require('handlebars');
+
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -36,14 +45,34 @@ handlebars.registerHelper('CFDAStringSlice', function(CFDA) {
   return CFDA.split(',')[0];
 })
 
+app.get('/logout', (req, res) => {
+  // Destroy the user session
+  req.session.destroy(err => {
+    if (err) {
+      console.error('Error destroying session:', err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      // Redirect to the login page or any other appropriate page
+      res.redirect('/login'); 
+    }
+  });
+});
+
 app.get('/dashboard', (req, res) => {
- // const data = [];
+
+ if (!req.session.email) {
+  return res.redirect('/login'); // Redirect to login if not logged in
+}
+
+// Access the email from the session
+  const userEmail = req.session.email;
+
   const sql = 'SELECT * from grants_trackings';
   connection.query(sql, (err,results) => {
     console.log("results in render",results.length);
-    res.render('dashboard', { data:results });
+    res.render('dashboard', { data:results,userEmail });
   })
-  //res.render('dashboard', { data }); // Render the dashboard with no results initially
+
 });
 
 
@@ -68,44 +97,135 @@ app.post('/dashboard', (req,res) => {
         if(err){
           res.send("Error executing SQL query: ' + error.stack");
         }else{
-          console.log("results",results.length);
-          res.render('dashboard',{ data: results });
+          res.render('dashboard',{data: results});
         }
     });
 });
 
+
+// Enable TFA for a user
+app.get('/enable-tfa', (req, res) => {
+
+  if (!req.session.email) {
+    return res.redirect('/login');
+  }
+
+  const email = req.session.email;
+
+  const secretKey = speakeasy.generateSecret({length: 20});
+
+  //  Generate QR code for the secret key
+  // const qrCodeURL = QRCode.toDataURL(secretKey.otpauth_url);
+
+  // Save the QR code image to a file (you may need to adjust the file path)
+  const qrCodeImagePath = 'C:/Users/admin/Downloads/Grants-Portal/src/frontend/public/qr-code.png';
+  QRCode.toFile(qrCodeImagePath, secretKey.otpauth_url);
+
+  const qrCodeURL = 'C:/Users/admin/Downloads/Grants-Portal/src/frontend/public/qr-code.png';
+
+  const sql = 'UPDATE users_2 SET is_tfa_enabled = 1, secret_key = ? WHERE email = ?';
+
+  connection.query(sql, [secretKey.base32, email], (err) => {
+    if (err) {
+      console.error(err.name);
+      res.status(500).send('Error enabling TFA');
+      return;
+    }
+    res.render('enable-tfa', {qrCodeURL: qrCodeURL});
+});
+});
+
+// TFA verification route
+app.post('/verify-tfa', (req, res) => {
+  const email = req.session.email;
+  const token = req.body.token;
+
+  // Retrieve secret key from database based on email
+  connection.query('SELECT secret_key FROM users_2 WHERE email = ?', [email], (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error verifying token');
+      return;
+    }
+
+    const secretKey = results[0].secret_key;
+
+  // Verify token using speakeasy
+  const verified = speakeasy.totp.verify({
+    secret: secretKey,
+    encoding: 'base32',
+    token: token,
+    window: 1
+  });
+
+  if (verified) {
+    req.session.userEmail = email;
+    res.redirect('/dashboard');
+  } else {
+    res.status(401).send({message: 'Invalid token'});
+  }
+});
+});
+
+
 app.post('/login', async (req,res) => {
-  const {username,password} = req.body;
+  const {email,password} = req.body;
+  
+  if (!isValidEmail(email)) {
+    return res.status(400).send('Invalid email format');
+  }
 
-  const sql = 'SELECT * from users where username = ? and password = ?';
+  const sql = 'SELECT * from users_2 where email = ? and password = ?';
 
-  connection.query(sql, [username,password], (err,results) => {
+  connection.query(sql, [email,password], (err,results) => {
       if(err){
           console.log("Database error",err)
           res.send("Login failed")
       }else{
           if(results.length>=1){
-              res.redirect('/dashboard');
+              const user = results[0];
+              const tfa_enabled = user.is_tfa_enabled;
+              // console.log("tfa_enabled",tfa_enabled)
+
+              req.session.email = req.body.email;
+              res.redirect('/enable-tfa');
+              // if (!user.is_tfa_enabled) {
+              
+              // } else {
+              //   req.session.email = email;
+              //   res.redirect('/dashboard');
+              // }
           }else{
-              res.send("Username and Password combination is incorrect");
+              res.send("Email and Password combination is incorrect");
           }
       }
   });
 })
 
 app.post('/register', async(req,res) => {
-  const {username,password} = req.body;
-  const sql = 'INSERT into users (username, password) values (?,?)';
+  const {email,password} = req.body;
 
-  connection.query(sql, [username, password], (err,results) => {
+  if (!isValidEmail(email)) {
+    return res.status(400).send('Invalid email format');
+  }
+
+  const sql = 'INSERT into users_2 (email, password) values (?,?)';
+
+  connection.query(sql, [email, password], (err,results) => {
     if(err){
       console.log("Database error",err);
+      res.send("Account already Exists");
     }else{
       res.redirect('/login');
     }
   });
 
 })
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
 
 app.get('/login', (req, res) => {
   res.render('login'); // Render the login page
