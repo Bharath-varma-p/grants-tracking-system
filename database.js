4a6ac36ec077128422f93a2da6b997584b4c8e51
penumaba <penumaba@mail.uc.edu>
2024-02-22 22:33:47 -0500

    fixed the pagenation issue

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'Pa$$w0rd',
-  database: 'loginsystem'
+  database: 'grants_portal'
   });
 
 module.exports=connection;
